c9eee3ecaa53e453f8364ba55dd03114c4af3911
TrungBui-Purdue-5913 <bui23@purdue.edu>
2021-10-09 18:14:18 -0400

    Nearly Finish React

--- part1/part1-learn/src/App.js
+++ part1/part1-learn/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+import React, { useState } from 'react'
+  /** 
+   * const name = props.name
+   * const age = props.age
+   * because we past down props like object:
+   * props = {
+   *   name: "Trung",
+   *   age : 19
+   * }
+   * We could use Destructing Assignment:
+   * const {name, age} = props
+   * we could even do it on the name of funtion:
+   * const Hello = ({name, age}) => 
+   
+  // Now we want to know the birth year of the user, we could use component helper function
+const Hello = ({name, age}) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - age
+  }
   return (
     <div>
-    <p>Hello, {name}</p>
-    <p>You are {age} years old</p>
+    <p>Hello, {name}. You are {age} years old</p>
+    <p>You were born in {bornYear()}</p>
     </div>
   )
 }
@@ -19,13 +36,93 @@ const Footer = () => {
 }
 const App = () => {
   const now = new Date()
-  const a = 10
-  const b = 20
   return (
     <div>
       <h1>Greeting</h1>
       <Hello name = "Trung" age = {19}/> 
       <Footer />
+    </div>
+  )
+}
+*/
+// Now we want to update a number every time the page is render, it will count
+// This is a bad example, we should change it by make everything become a component
+/**
+const App = () => {
+  let [counter, setCounter] = useState(0)
+
+  //setTimeout(
+    //() => setCounter(counter + 1),
+    //1000
+  //)
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+  }
+
+  const setToZero = () => {
+    setCounter(0)
+  }
+  console.log("rendering.....", counter)
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </div>
+  )
+}
+*/
+const Header = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({text, onClickHandler}) => {
+  return (
+    <>
+    <button onClick = {onClickHandler}>
+      {text}
+    </button>
+    </>
+  )
+}
+const App = () => {
+  let [ counter, setCounter ] = useState(0)
+  let [left, setLeft] = useState(0)
+  let [right, setRight] = useState(0)
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    setCounter(counter - 1)
+  }
+
+  const setZero = () => {
+    setCounter(0)
+  }
+  const changeLeft = () => {
+    setLeft(left + 1)
+  }
+  const changeRight = () => {
+    setRight(right + 1)
+  }
+  return (
+    <div>
+      <Header counter = {counter}/>
+      <Button text = "plus" onClickHandler={increaseByOne}/>
+      <Button text = "minus" onClickHandler={decreaseByOne}/>
+      <Button text = "reset" onClickHandler={setZero}/> 
+      <Header counter = "This is for multiple component update"/>
+      <Header counter = {left}/>
+      <Header counter = {right}/>
+      <Button text = "change left" onClickHandler={changeLeft}/>
+      <Button text = "change right" onClickHandler={changeRight}/>
     </div>
   )
 }
--- part1/part1-learn/src/index.js
+++ part1/part1-learn/src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+/** 
+let counter = 1
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App counter = {counter}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//change the index file will not re-render the page because this is functional programming
+// this function will refresh the page when called
+const refresh = () => {
+  ReactDOM.render(<App counter = {counter}/>,
+  document.getElementById('root'))
+}
+
+// This will update counter and rerender the page 1000 time, but
+// it is not a good idea to rerender the page every time we want to update it
+// The better way is to change the App.js file
+/** 
+ setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000);
+ */
+// We will need to reinitialize everything
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
